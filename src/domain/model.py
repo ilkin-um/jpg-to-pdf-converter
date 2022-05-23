d7c6dff8c3172a3704386f1e5bce4310b4b4de25
ilkin-um <ilkinum@gmail.com>
2022-05-23 16:38:06 +0400

    created pdf dataclass and added some tests

--- src/domain/model.py
+++ src/domain/model.py
@@ -5,3 +5,17 @@ from dataclasses import dataclass, field
 class JPG:
     src_path: str
     extensions: tuple[str, str] = field(init=False, default=(".jpg", ".jpeg"))
+
+
+@dataclass
+class PDF:
+    destination_path: str
+    extension: str = field(init=False, default=(".pdf"))
+
+
+def allocate_jpeg(src_path: str) -> JPG:
+    return JPG(src_path=src_path)
+
+
+def allocate_pdf(destination_path: str) -> PDF:
+    return PDF(destination_path=destination_path)
--- test/test_models.py
+++ test/test_models.py
@@ -10,8 +10,30 @@ def test_jpeg_creation():
     assert jpeg.extensions == (".jpg", ".jpeg")
 
 
-def test_jpegs_are_equal():
+def test_if_jpegs_are_equal():
     jpg1 = model.JPG(src_path="test.jpg")
     jpg2 = model.JPG(src_path="test.jpg")
 
     assert jpg1 == jpg2
+
+
+def test_pdf_creation():
+    pdf = model.PDF(destination_path="test.pdf")
+    assert pdf.extension == (".pdf")
+
+
+def test_if_jpdfs_are_equal():
+    pdf1 = model.PDF(destination_path="test.pdf")
+    pdf2 = model.PDF(destination_path="test.pdf")
+
+    assert pdf1 == pdf2
+
+
+def test_jeg_allocation():
+    jpeg = model.JPG(src_path="test.jpg")
+    assert model.allocate_jpeg(src_path="test.jpg") == jpeg
+
+
+def test_pdf_allocation():
+    pdf = model.PDF(destination_path="test.pdf")
+    assert model.allocate_pdf(destination_path="test.pdf") == pdf
