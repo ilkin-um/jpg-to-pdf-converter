95fa77dcfcde8d58ce29c7c621a47adbdef1c5ca
ilkin-um <ilkinum@gmail.com>
2022-05-24 17:15:21 +0400

    added more tests

--- src/service/validator.py
+++ src/service/validator.py
@@ -6,7 +6,7 @@ def is_jpg_path_valid(func):
     @wraps(func)
     def wrapper(jpg_path: str, _: str):
         if not os.path.isfile(jpg_path):
-            raise FileNotFoundError("Path does not include jpeg file")
+            raise FileNotFoundError("Path to jpeg file is wrong")
         return func(jpg_path, _)
 
     return wrapper
@@ -16,7 +16,7 @@ def is_pdf_path_valid(func):
     @wraps(func)
     def wrapper(pdf_path):
         if not os.path.exists(os.path.dirname(pdf_path)):
-            raise FileNotFoundError("Wrong pdf file path provided")
+            raise FileNotFoundError("Path to pdf file is wrong")
         return func(pdf_path)
 
     return wrapper
--- test/test_service.py
+++ test/test_service.py
@@ -1,5 +1,7 @@
 import os
+import pytest
 from src.service import converter
+from src.domain.exception import WrongFileExtensionException
 
 
 def test_jpeg_to_pdf_conversion():
@@ -7,3 +9,27 @@ def test_jpeg_to_pdf_conversion():
     pdf_path = os.path.abspath("test/data/random.pdf")
     converter.jpg2pdf(jpg_path, pdf_path)
     assert os.path.isfile(pdf_path)
+
+
+def test_with_wrong_jpg_path():
+    jpg_path = os.path.abspath("./notexists/random.jpg")
+    pdf_path = os.path.abspath("test/data/random.pdf")
+    with pytest.raises(FileNotFoundError) as err:
+        converter.jpg2pdf(jpg_path, pdf_path)
+    assert str(err.value) == "Path to jpeg file is wrong"
+
+
+def test_with_wrong_pdf_path():
+    jpg_path = os.path.abspath("test/data/random.jpg")
+    pdf_path = os.path.abspath("test/notexists/random.pdf")
+    with pytest.raises(FileNotFoundError) as err:
+        converter.jpg2pdf(jpg_path, pdf_path)
+    assert str(err.value) == "Path to pdf file is wrong"
+
+
+def test_with_wrong_format():
+    jpg_path = os.path.abspath("test/data/random.txt")
+    pdf_path = os.path.abspath("test/asdsad/random.pdf")
+    with pytest.raises(WrongFileExtensionException) as err:
+        converter.jpg2pdf(jpg_path, pdf_path)
+    assert str(err.value) == "Expected file with .jpg or .jpeg extension, got another."
