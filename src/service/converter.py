67197739cfbc87a3b9b48da80deb878d02ffcf21
ilkin-um <ilkinum@gmail.com>
2022-05-24 16:27:40 +0400

    created converter functions

--- src/service/converter.py
+++ src/service/converter.py
@@ -5,6 +5,19 @@ from contextlib import contextmanager
 from src.domain import model
 
 
+def _convert_and_save_pd(image, pdf):
+    pdf_bytes = img2pdf.convert(image.filename)
+
+    with _create_pdf(pdf.destination_path, pdf_bytes) as file:
+        file.write(pdf_bytes)
+
+
+def _get_jpg_and_pdf(jpg_path, pdf_path):
+    jpg = model.JPG(src_path=jpg_path)
+    pdf = model.PDF(destination_path=pdf_path)
+    return jpg, pdf
+
+
 @contextmanager
 def _open_jpg(img_path: str):
     try:
@@ -15,7 +28,7 @@ def _open_jpg(img_path: str):
 
 
 @contextmanager
-def _create_df(pdf_path: str, pdf_bytes: bytes):
+def _create_pdf(pdf_path: str, pdf_bytes: bytes):
     try:
         file = open(pdf_path, "wb")
         yield file
