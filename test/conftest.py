59e417ec982c4cbacd0638225ec055d62ec41486
ilkin-um <ilkinum@gmail.com>
2022-05-31 17:22:42 +0400

    mounted project for further imports:

--- test/conftest.py
+++ test/conftest.py
@@ -1,11 +1,15 @@
 import pytest
 import uuid
+import sys
+
+sys.path.append(r"C:\Users\pythondev\Desktop\personal\portfolio\jpeg-to-pdf-converter")
 from src.entrypoints.api.app import create_app
 
 
 @pytest.fixture
 def app():
     return create_app("testing")
+
 
 @pytest.fixture
 def get_uuid():
