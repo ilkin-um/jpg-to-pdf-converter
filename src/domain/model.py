2c67b2b8e50037b6f1d444b3317a7efd8f2972a1
ilkin-um <ilkinum@gmail.com>
2022-06-02 11:58:10 +0400

    added jpeg creation from dict

--- src/domain/model.py
+++ src/domain/model.py
@@ -1,12 +1,20 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, asdict
 from src.domain.validator import is_jpeg, is_pdf
 import uuid
+
 
 @dataclass
 class JPG:
     code: uuid.UUID
     src_path: str
     extensions: tuple[str, str] = field(init=False, default=(".jpg", ".jpeg"))
+
+    @classmethod
+    def from_dict(cls, dict_):
+        return cls(**dict_)
+
+    def to_dict(self):
+        return asdict(self)
 
 
 @dataclass
@@ -18,9 +26,9 @@ class PDF:
 
 @is_jpeg
 def allocate_jpeg(code: uuid.UUID, src_path: str) -> JPG:
-    return JPG(code=code,src_path=src_path)
+    return JPG(code=code, src_path=src_path)
 
 
 @is_pdf
-def allocate_pdf(code: uuid.UUID,destination_path: str) -> PDF:
-    return PDF(code=code,destination_path=destination_path)
+def allocate_pdf(code: uuid.UUID, destination_path: str) -> PDF:
+    return PDF(code=code, destination_path=destination_path)
--- test/test_models.py
+++ test/test_models.py
@@ -1,54 +1,70 @@
 import pytest
 import sys
 import uuid
+
 sys.path.append(r"C:\Users\pythondev\Desktop\personal\portfolio\jpeg-to-pdf-converter")
 from src.domain import model
 from src.domain.exception import WrongFileExtensionException
 
 
 def test_jpeg_creation(get_uuid):
-    jpeg = model.JPG(code=get_uuid,src_path="test.jpg")
+    jpeg = model.JPG(code=get_uuid, src_path="test.jpg")
     assert jpeg.extensions == (".jpg", ".jpeg")
 
 
 def test_if_jpegs_are_equal(get_uuid):
-    jpg1 = model.JPG(code=get_uuid,src_path="test.jpg")
-    jpg2 = model.JPG(code=get_uuid,src_path="test.jpg")
+    jpg1 = model.JPG(code=get_uuid, src_path="test.jpg")
+    jpg2 = model.JPG(code=get_uuid, src_path="test.jpg")
 
     assert jpg1 == jpg2
 
 
 def test_pdf_creation(get_uuid):
-    pdf = model.PDF(code=get_uuid,destination_path="test.pdf")
+    pdf = model.PDF(code=get_uuid, destination_path="test.pdf")
     assert pdf.extension == (".pdf")
 
 
 def test_if_pdfs_are_equal(get_uuid):
-    pdf1 = model.PDF(code=get_uuid,destination_path="test.pdf")
-    pdf2 = model.PDF(code=get_uuid,destination_path="test.pdf")
+    pdf1 = model.PDF(code=get_uuid, destination_path="test.pdf")
+    pdf2 = model.PDF(code=get_uuid, destination_path="test.pdf")
 
     assert pdf1 == pdf2
 
 
 def test_jpeg_allocation(get_uuid):
-    jpeg = model.JPG(code=get_uuid,src_path="test.jpg")
-    assert model.allocate_jpeg(code=get_uuid,src_path="test.jpg") == jpeg
+    jpeg = model.JPG(code=get_uuid, src_path="test.jpg")
+    assert model.allocate_jpeg(code=get_uuid, src_path="test.jpg") == jpeg
 
 
 def test_pdf_allocation(get_uuid):
-    pdf = model.PDF(code=get_uuid,destination_path="test.pdf")
-    assert model.allocate_pdf(code=get_uuid,destination_path="test.pdf") == pdf
+    pdf = model.PDF(code=get_uuid, destination_path="test.pdf")
+    assert model.allocate_pdf(code=get_uuid, destination_path="test.pdf") == pdf
 
 
 def test_jpeg_allocation_wrong_extension(get_uuid):
     with pytest.raises(WrongFileExtensionException) as err:
-        model.allocate_jpeg(code=get_uuid,src_path="test.mp4")
+        model.allocate_jpeg(code=get_uuid, src_path="test.mp4")
 
     assert str(err.value) == "Expected file with .jpg or .jpeg extension, got another."
 
 
 def test_pdf_allocation_wrong_extension(get_uuid):
     with pytest.raises(WrongFileExtensionException) as err:
-        model.allocate_pdf(code=get_uuid,destination_path="test.svg")
+        model.allocate_pdf(code=get_uuid, destination_path="test.svg")
 
     assert str(err.value) == "Expected file with .pdf extension, got another."
+
+
+def test_jpeg_model_from_dict(get_uuid):
+    di = {"code": get_uuid, "src_path": "random.jpg"}
+    pdf = model.JPG.from_dict(di)
+
+    assert pdf.code == get_uuid
+    assert pdf.src_path == "random.jpg"
+
+
+def test_jpeg_model_to_dict(get_uuid):
+    di = {"code": get_uuid, "src_path": "random.jpg"}
+    jpg = model.JPG.from_dict(di)
+    di["extensions"] = jpg.extensions
+    assert jpg.to_dict() == di
