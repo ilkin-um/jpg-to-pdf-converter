5304be60f470fda67ec313e04525138569493c9e
ilkin-um <ilkinum@gmail.com>
2022-06-03 12:26:44 +0400

    created memory repo

--- test/test_use_cases.py
+++ test/test_use_cases.py
@@ -3,7 +3,7 @@ from unittest import mock
 from src.use_cases.converted_list import converted_list
 
 
-def test_converted_list_without_parameters(converted_data):
+def test_converted_list(converted_data):
     repo = mock.Mock()
     repo.list.return_value = converted_data
     result = converted_list(repo)
